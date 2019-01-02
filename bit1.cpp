376d3b984d5a8dcb0fd7b9eb8973a04613ca984b
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-02 10:38:52 +0530

    Add files via upload

---
+++ bit1.cpp
@@ -0,0 +1,13 @@
+// Program to Check Whether the Number is Even or Odd
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int n=34;
+	if(n&1) cout<<"Odd";
+	else cout<<"Even";
+
+	return 0;
+}
