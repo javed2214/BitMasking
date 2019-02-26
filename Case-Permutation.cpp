91bb8d88262af2235df18886b4ce3c370a747672
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-27 00:21:23 +0530

    Add files via upload

---
+++ Case-Permutation.cpp
@@ -0,0 +1,35 @@
+// Permute A String by Changing Case
+// ab => ab,aB,Ab,AB
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void getBit(string str, int no){
+
+	int i=0;
+	while(no){
+		if(no&1) str[i]=toupper(str.at(i));
+		i++;
+		no>>=1;
+	}
+	cout<<str<<" ";
+	return;
+}
+
+void getMask(string str){
+
+	int range=1<<str.length();
+	transform(str.begin(),str.end(),str.begin(),::tolower);
+
+	for(int i=0;i<=range-1;i++)
+		getBit(str,i);
+	return;
+}
+
+int main(){
+
+	string str="abc";
+	getMask(str);
+
+	return 0;
+}
